14c48f25f919824e55f8440e67bc25670c200c37
CrazyBunQnQ <baobao222222@qq.com>
2017-05-08 21:42:35 +0800

    增加岗位管理的 DAO 层及其实现（包含岗位权限）

--- src/com/bank/dao/GwymDao.java
+++ src/com/bank/dao/GwymDao.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public interface GwymDao {
 
 	/**
-	 * 查询指定岗位的权限
+	 * 查询指定岗位的可操作页面编号、可操作页面和可操作模块
 	 * @param conn
 	 * @param jobId 岗位 id
 	 * @param page 当前页
@@ -17,5 +17,57 @@ public interface GwymDao {
 	 */
 	ResultSet queryGwyms(Connection conn, int jobId, int page, int count) throws SQLException;
 	
+	/**
+	 * 查询指定岗位、指定操作模块的可操作页面编号和名称
+	 * @param conn
+	 * @param jobId 岗位 id
+	 * @param funId 模块 id
+	 * @param page 当前页
+	 * @param count 每页显示数量
+	 * @return
+	 * @throws SQLException
+	 */
+	ResultSet queryGwyms(Connection conn, int jobId, int funId, int page, int count) throws SQLException;
 	
+	/**
+	 * 按照指定操作模块查询该模块所有的操作页面编号和名称
+	 * @param conn
+	 * @param funId 模块 id
+	 * @param page 当前页
+	 * @param count 每页显示数量
+	 * @return
+	 * @throws SQLException
+	 */
+	ResultSet queryAllGwyms(Connection conn, int funId, int page, int count) throws SQLException;
+	
+	/**
+	 * 给指定岗位添加指定的子功能
+	 * @param conn
+	 * @param jobId 岗位 id
+	 * @param ymbh 子功能 id
+	 * @return int 影响行数
+	 * @throws SQLException
+	 */
+	@Deprecated
+	int insertGwym(Connection conn, int jobId, int ymbh) throws SQLException;
+	
+	/**
+	 * 给指定岗位添加指定的子功能数组
+	 * @param conn
+	 * @param jobId 岗位 id
+	 * @param ymbh 子功能 id 数组
+	 * @return int 影响行数
+	 * @throws SQLException
+	 */
+	int insertGwyms(Connection conn, int jobId, int[] ymbh) throws SQLException;
+	
+	/**
+	 * 删除指定岗位的某些子功能
+	 * @param conn
+	 * @param jobId 岗位 id
+	 * @param ymbh 子功能 id 数组
+	 * @return
+	 * @throws SQLException
+	 */
+	int deleteGwyms(Connection conn, int jobId, int[] ymbh) throws SQLException;
 }
--- src/com/bank/dao/impl/GwymDaoImpl.java
+++ src/com/bank/dao/impl/GwymDaoImpl.java
@@ -9,6 +9,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.bank.dao.GwymDao;
+import com.bank.util.DBUtil;
 
 public class GwymDaoImpl implements GwymDao {
 
@@ -16,7 +17,7 @@ public class GwymDaoImpl implements GwymDao {
 	
 	@Override
 	public ResultSet queryGwyms(Connection conn,int jobId, int page, int count) throws SQLException {
-		PreparedStatement ps = conn.prepareStatement("SELECT job.Job_ID, xtymb.ymbh FROM gwym, job, xtymb WHERE job.Job_ID=? AND job.Job_ID=gwym.Job_ID AND xtymb.ymbh=gwym.ymbh LIMIT ?, ?");
+		PreparedStatement ps = conn.prepareStatement("SELECT xtymb.ymbh, xtymb.ymmc, functions.Func_Name FROM gwym, job, xtymb, functions WHERE job.Job_ID=? AND job.Job_ID=gwym.Job_ID AND xtymb.ymbh=gwym.ymbh AND functions.Func_ID=xtymb.Func_ID LIMIT ?, ?");
 		ps.setInt(1, jobId);
 		ps.setInt(2, page);
 		ps.setInt(3, count);
@@ -24,4 +25,67 @@ public class GwymDaoImpl implements GwymDao {
 		return ps.executeQuery();
 	}
 
+	@Override
+	public ResultSet queryGwyms(Connection conn, int jobId, int funId, int page, int count) throws SQLException {
+		PreparedStatement ps = conn.prepareStatement("SELECT xtymb.ymbh, xtymb.ymmc FROM gwym, job, xtymb, functions WHERE job.Job_ID=? AND functions.Func_ID=? AND job.Job_ID=gwym.Job_ID AND xtymb.ymbh=gwym.ymbh AND functions.Func_ID=xtymb.Func_ID ORDER BY xtymb.ymbh LIMIT ?, ?");
+		ps.setInt(1, jobId);
+		ps.setInt(2, funId);
+		ps.setInt(3, page);
+		ps.setInt(4, count);
+		LOGGER.info("查询指定岗位、指定模块的权限：" + ps.toString());
+		return ps.executeQuery();
+	}
+	
+	public ResultSet queryAllGwyms(Connection conn, int funId, int page, int count) throws SQLException {
+		PreparedStatement ps = conn.prepareStatement("SELECT ymbh, ymmc FROM xtymb WHERE Func_ID=? ORDER BY ymbh LIMIT ?, ?");
+		ps.setInt(1, funId);
+		ps.setInt(2, page);
+		ps.setInt(3, count);
+		LOGGER.info("查询指定模块的权限：" + ps.toString());
+		return ps.executeQuery();
+	}
+
+	@Override
+	@Deprecated
+	public int insertGwym(Connection conn, int jobId, int ymbh) throws SQLException {
+		PreparedStatement ps = conn.prepareStatement("INSERT INTO gwym VALUES (?, ?)");
+		ps.setInt(1, jobId);
+		ps.setInt(2, ymbh);
+		LOGGER.info("给岗位 " + jobId + " 添加 " + ymbh + " 功能：" + ps.toString());
+		return DBUtil.closeStatement(ps);
+	}
+
+	@Override
+	public int insertGwyms(Connection conn, int jobId, int[] ymbh) throws SQLException {
+		// TODO 将 sql 变成参数传进来
+		StringBuffer sql = new StringBuffer();
+		int n = ymbh.length;
+		for (int i = 0; i < n; i++) {
+			sql.append("INSERT INTO gwym VALUES (?, ?);");
+		}
+		PreparedStatement ps = conn.prepareStatement(sql.toString());
+		for (int i = 0; i < n; i++) {
+			ps.setInt(2 * i + 1, jobId);
+			ps.setInt(2 * (i + 1), ymbh[i]);
+		}
+		LOGGER.info("给岗位 " + jobId + " 添加 " + n + " 个功能：" + ps.toString());
+		return DBUtil.closeStatement(ps);
+	}
+
+	@Override
+	public int deleteGwyms(Connection conn, int jobId, int[] ymbh) throws SQLException {
+		// TODO 将 sql 变成参数传进来
+		StringBuffer sql = new StringBuffer();
+		int n = ymbh.length;
+		for (int i = 0; i < n; i++) {
+			sql.append("DELETE FROM gwym WHERE Job_ID=? AND ymbh=?;");
+		}
+		PreparedStatement ps = conn.prepareStatement(sql.toString());
+		for (int i = 0; i < n; i++) {
+			ps.setInt(2 * i + 1, jobId);
+			ps.setInt(2 * (i + 1), ymbh[i]);
+		}
+		LOGGER.info("给岗位 " + jobId + " 删除 " + n + " 个功能：" + ps.toString());
+		return DBUtil.closeStatement(ps);
+	}
 }
