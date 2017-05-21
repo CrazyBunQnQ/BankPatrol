3c5ab3ac222e42d672c395a5b26b4788b6a48531
CrazyBunQnQ <baobao222222@qq.com>
2017-05-21 21:22:34 +0800

    删除 LoginController 和 UserController

--- src/com/bank/controller/UserController.java
+++ src/com/bank/controller/UserController.java
@@ -1,32 +1,4 @@
 package com.bank.controller;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import com.bank.service.UserService;
-import com.bank.service.impl.UserServiceImpl;
-
 public class UserController {
-	UserService us = new UserServiceImpl();
-
-	/**
-	 * 检查用户名是否存在
-	 * 
-	 * @param request
-	 * @param response
-	 */
-	public void checkUName(HttpServletRequest request, HttpServletResponse response) {
-		String uname = request.getParameter("users.loginId");
-		try {
-			if (us.hasUser(uname)) {
-				response.getWriter().write("1");
-			} else {
-				response.getWriter().write("0");
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
 }
