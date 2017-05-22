54c632719c695d4d2f435930c69168284e4d80af
CrazyBunQnQ <baobao222222@qq.com>
2017-05-22 16:55:37 +0800

    添加 Action 基类

---
+++ src/com/bank/action/BaseAction.java
@@ -0,0 +1,36 @@
+package com.bank.action;
+
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.struts2.interceptor.ApplicationAware;
+import org.apache.struts2.interceptor.RequestAware;
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class BaseAction extends ActionSupport implements RequestAware, SessionAware, ApplicationAware {
+
+	private static final long serialVersionUID = 8561632042773802477L;
+	protected static final Logger log = LogManager.getLogger(BaseAction.class.getName());
+
+	protected Map<String, Object> application;
+	protected Map<String, Object> session;
+	protected Map<String, Object> request;
+
+	@Override
+	public void setApplication(Map<String, Object> application) {
+		this.application = application;
+	}
+
+	@Override
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
+	}
+
+	@Override
+	public void setRequest(Map<String, Object> request) {
+		this.request = request;
+	}
+}
